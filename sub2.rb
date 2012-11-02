aebea64237a20fd6c8a0aaced01108d2ce4aa81c
pongadong <pongadong2@gmail.com>
2012-11-02 13:00:42 +0100

    deaf grandmom exercize

---
+++ sub2.rb
@@ -0,0 +1,17 @@
+i=3
+puts 'talk to your grandmom'
+while i>0
+  i_say = gets.chomp
+  if i_say != i_say.upcase || i_say == 'BYE' || i_say == ''
+    then 
+    puts 'HUH?! SPEAK UP, SONNY!'
+    if i_say == 'BYE'
+        i = i-1
+      else
+        i=3
+      end
+  else 
+    puts('NO, NOT SINCE ' + (1930 + rand(21)).to_s + ' !')
+  end  
+end
+puts 'OH, BYE THEN!'
